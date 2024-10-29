9f97a1f0875d46f0ff0aa96f8db555b7fd27593e
NickMarie <00001101824189sp@al.educacao.sp.gov.br>
2024-10-29 08:49:04 -0300

    Flashcards

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Animais',
+    'Qual é o maior animal do mundo?',
+    'O maior animal do mundo é a baleia-azul, que pode atingir até 30 metros de comprimento e pesar mais de 180 toneladas.'
+)
+
+criaCartao(
+    'Animais',
+    'Qual é o animal mais rápido em terra?',
+    'O animal mais rápido em terra é o guepardo, que pode correr a uma velocidade de até 120 km/h em distâncias curtas.'
+)
+
+criaCartao(
+    'Animais',
+    'Qual é o único mamífero capaz de voar?',
+    'O único mamífero que pode voar são os morcegos. Eles têm asas feitas de pele esticada entre os dedos, o que permite que voem.'
+)
+
+criaCartao(
+    'Animais',
+    'Quantos corações tem um polvo?',
+    'Um polvo tem três corações. Dois bombeiam sangue para as brânquias, e o terceiro bombeia para o resto do corpo.'
+)
